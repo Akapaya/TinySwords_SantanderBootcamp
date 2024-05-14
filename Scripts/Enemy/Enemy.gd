f2f3616980dcd3e85ee01a5c8598ac1e14a1092f
Akapaya <63330951+Akapaya@users.noreply.github.com>
2024-05-14 17:13:59 -0300

    Implemented Damage Feedback
    Implemented Damage Feedback

--- Scripts/Enemy/Enemy.gd
+++ Scripts/Enemy/Enemy.gd
@@ -3,15 +3,28 @@ extends CharacterBody2D
 
 @export var health: int = 10
 @export var deathFab: PackedScene
+var damageDigitFab: PackedScene
+var damageDigitMarker: Marker2D
 
 var healthManager : HealthManager
 
 func _ready():
 	healthManager = get_node("HealthManager")
+	damageDigitFab = preload("res://Animations/DamageDigit.tscn")
+	damageDigitMarker = $DamageDigitMarker
 
 func TakeDamage(damage: int) -> void:
 	health = healthManager.TakeDamage(damage, health)
 	DamageEffect()
+		
+	var damageDigit = damageDigitFab.instantiate()
+	damageDigit.value = damage
+	if damageDigitMarker:
+		damageDigit.global_position = damageDigitMarker.global_position
+	else:
+		damageDigit.global_position = global_position
+	
+	get_parent().add_child(damageDigit)
 	
 	if health <= 0:
 		DeathAnimation()
